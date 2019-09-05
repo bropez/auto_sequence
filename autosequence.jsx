8605e87b59b6ba17165c48790f513605efb9b8af
Matthew <matthewbropez@gmail.com>
2019-09-05 15:05:48 -0400

    Added some documentation to autosequence.jsx

--- autosequence.jsx
+++ autosequence.jsx
@@ -1,4 +1,10 @@
-﻿function get_clip(my_name, my_array) {
+﻿/*
+    * Loops through the given array to find the object with the given name
+    * @param    {String}                                          The name of the desired clip
+    * @param    {app.project.rootItem.children} The "array" that holds all of the clips in the project explorer
+    * @return      {app.project.rootItem[j]}             The desired clip
+*/
+function get_clip(my_name, my_array) {
     for (j = 0; j < my_array.numItems; j++) {
         if (my_array[j].name === my_name) {
             return my_array[j];
@@ -11,6 +17,7 @@ var color_bars = get_clip("color_bars.mp4", app.project.rootItem.children);
 var track = activeSeq.videoTracks[1];
 var cb_track = activeSeq.videoTracks[2];
 
+// looping through the project panel minus an added sequence and "color_bars.mp4"
 for (i = 1; i < app.project.rootItem.children.numItems - 2; i++) {
     var current_name = "comment" + i + ".mp4";
     var clip = get_clip(current_name, app.project.rootItem.children);
